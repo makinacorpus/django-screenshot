0442d9f0adb713f0d0c525f5435f98d2683fe008
J-E Castagnede <j.e.castagnede@gmail.com>
2019-03-25 20:14:08 +0100

    100% tests

--- src/screenshotter/tests.py
+++ src/screenshotter/tests.py
@@ -3,12 +3,16 @@ import magic
 from tempfile import TemporaryDirectory
 
 from PIL import Image
+from django.contrib.auth.models import User
 from django.core.files.base import ContentFile
 from django.core.files.storage import default_storage
 from django.test import TestCase, override_settings
+from django.urls import reverse
 from rest_framework.serializers import Serializer
+from rest_framework.test import APIClient
 
 from screenshotter.helpers import call_puppeteer
+from screenshotter.serializer import ScreenshotSerializer
 from screenshotter.views import CaptureAPIView
 
 temp_dir = TemporaryDirectory()
@@ -53,3 +57,30 @@ class CaptureTestCase(TestCase):
 
         self.assertTrue(hasattr(view, 'get_serializer'))
         self.assertTrue(isinstance(view.get_serializer(), Serializer))
+
+
+class CaptureApiTestCase(TestCase):
+    def setUp(self):
+        self.api_client = APIClient()
+        self.user = User.objects.create(username='test')
+        self.api_client.force_login(self.user)
+
+    def test_api_good_request(self):
+        serializer = ScreenshotSerializer()
+        data = serializer.data
+        data['url'] = "https://www.google.fr"
+
+        response = self.api_client.post(reverse('screenshotter:capture'), data=data, format='json')
+        self.assertEqual(response.status_code, 200, serializer.data)
+
+    def test_api_bad_request(self):
+        serializer = ScreenshotSerializer()
+        response = self.api_client.post(reverse('screenshotter:capture'), data=serializer.data, format='json')
+        self.assertEqual(response.status_code, 400, serializer.data)
+
+    def test_api_wrong_response(self):
+        serializer = ScreenshotSerializer()
+        data = serializer.data
+        data['url'] = "https://kikou.com"
+        response = self.api_client.post(reverse('screenshotter:capture'), data=data, format='json')
+        self.assertEqual(response.status_code, 500, response.json())
